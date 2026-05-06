54d881a6b680c2e196db45c6bec45af592c84a04
Manoj Kumar <manojmahendia@gmail.com>
2026-05-06 13:56:21 +0530

    made the trip_weaver as a python package, now it ccan be treated as python packge."

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trip_weaver',
+    version='0.1.0',
+    author="Manoj Kumar",
+    author_email="manojmahendia@gmail.com",
+    packages=find_packages()
+)

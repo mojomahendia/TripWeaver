eeb44d8c731d25344b5256babbe1180da89cc698
Manoj Kumar <manojmahendia@gmail.com>
2026-05-07 15:29:54 +0530

    creating agents v1

---
+++ trip_weaver/agents/agents.py
@@ -0,0 +1,12 @@
+from langgraph.graph import START, END, StateGraph
+from typing import TypedDict
+from trip_weaver.models.gpt_model import model_client
+
+## State
+
+
+# create nodes
+
+# create graph
+
+# compile graph workflow
